730f426891feace1d6f158fc4952da610555a822
ExtendedGuru4883 <152730089+ExtendedGuru4883@users.noreply.github.com>
2025-06-13 23:10:46 +0200

    Used new ApiClient to handle api requests in Login, Signup pages

--- Client/Pages/Login.razor.cs
+++ Client/Pages/Login.razor.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components;
 using System.Text;
 using Blazored.LocalStorage;
 using Client.Models;
+using Client.Services.Clients.Interfaces;
 using Client.Services.Crypto.Interfaces;
 using Shared.Dto.Requests;
 using Shared.Dto.Responses;
@@ -11,48 +12,74 @@ namespace Client.Pages;
 
 public partial class Login : ComponentBase
 {
-    [Inject]
-    public required HttpClient HttpClient { get; set; }
-    [Inject]
-    public required ICryptoService CryptoService { get; set; }
-    [Inject]
-    public required ISignatureService SignatureService { get; set; }
-    [Inject]
-    public required ILocalStorageService LocalStorageService { get; set; }
-    
+    [Inject] public required IApiClient ApiClient { get; set; }
+    [Inject] public required ICryptoService CryptoService { get; set; }
+    [Inject] public required ISignatureService SignatureService { get; set; }
+    [Inject] public required ILocalStorageService LocalStorageService { get; set; }
+    [Inject] public required NavigationManager NavigationManager { get; set; }
+
     private readonly LoginFormModel _model = new();
     private List<string> _errors = [];
-    
+    private bool _isLoading = false;
 
-    private async Task Submit()
+
+    private async Task SubmitAsync()
     {
         _errors.Clear();
+        _isLoading = true;
 
-        var response = await HttpClient.GetAsync($"auth/GetChallenge?username={_model.Username}");
-        if (!response.IsSuccessStatusCode) return;
+        try
+        {
+            var apiChallengeResponse =
+                await ApiClient.HandleJsonGetAsync<ChallengeResponse>(
+                    $"auth/GetChallenge?username={_model.Username}");
 
-        var challengeResponse = await response.Content.ReadFromJsonAsync<ChallengeResponse>();
+            if (apiChallengeResponse is { IsSuccess: true, Data: not null })
+            {
+                var signatureBase64 = GetBase64DetachedChallengeSignature(apiChallengeResponse.Data);
+
+                var loginRequest = new LoginRequest()
+                {
+                    Username = _model.Username,
+                    NonceBase64 = apiChallengeResponse.Data.NonceBase64,
+                    NonceSignatureBase64 = signatureBase64,
+                };
+
+                var apiLoginResponse =
+                    await ApiClient.HandleJsonPostAsync<LoginResponse>("auth/login",
+                        JsonContent.Create(loginRequest));
+
+                if (apiLoginResponse is { IsSuccess: true, Data: not null })
+                {
+                    await LocalStorageService.SetItemAsStringAsync("token",
+                        apiLoginResponse.Data.Token);
+                    return;
+                }
+
+                //!apiLoginResponse.IsSuccess
+                _errors.Add(apiLoginResponse.ErrorMessage ?? "Unexpected error during login");
+                return;
+            }
+
+            //!apiChallengeResponse.IsSuccess
+            _errors.Add(apiChallengeResponse.ErrorMessage ?? "Unexpected error retrieving challenge");
+            NavigationManager.NavigateTo("/");
+        }
+        finally
+        {
+            _isLoading = false;
+        }
+    }
+
+    private string GetBase64DetachedChallengeSignature(ChallengeResponse challengeResponse)
+    {
         var nonceBytes = Convert.FromBase64String(challengeResponse.NonceBase64);
         var signatureSaltBytes = Convert.FromBase64String(challengeResponse.SignatureSaltBase64);
-        
+
         var passwordBytes = Encoding.UTF8.GetBytes(_model.Password);
 
         var keyPairBytes = SignatureService.GenerateKeyPair(passwordBytes, signatureSaltBytes, 32);
 
-        var signatureBytes = SignatureService.SignDetached(nonceBytes, keyPairBytes.PrivateKey);
-
-        var loginRequest = new LoginRequest()
-        {
-            Username = _model.Username,
-            NonceBase64 = challengeResponse.NonceBase64,
-            NonceSignatureBase64 = Convert.ToBase64String(signatureBytes)
-        };
-
-        response = await HttpClient.PostAsync("auth/login", JsonContent.Create(loginRequest));
-        var loginResponse = await response.Content.ReadFromJsonAsync<LoginResponse>();
-
-        if (!response.IsSuccessStatusCode) return;
-        
-        await LocalStorageService.SetItemAsStringAsync("token", loginResponse.Token);
+        return Convert.ToBase64String(SignatureService.SignDetached(nonceBytes, keyPairBytes.PrivateKey));
     }
 }
--- Client/Pages/Signup.razor.cs
+++ Client/Pages/Signup.razor.cs
@@ -2,6 +2,7 @@ using System.Net.Http.Json;
 using Microsoft.AspNetCore.Components;
 using System.Text;
 using Client.Models;
+using Client.Services.Clients.Interfaces;
 using Client.Services.Crypto.Interfaces;
 using Shared.Dto.Requests;
 
@@ -9,33 +10,51 @@ namespace Client.Pages;
 
 public partial class Signup : ComponentBase
 {
-    [Inject]
-    public required HttpClient HttpClient { get; set; } 
-    [Inject]
-    public required ICryptoService CryptoService { get; set; }
-    [Inject]
-    public required ISignatureService SignatureService { get; set; }
-    
+    [Inject] public required IApiClient ApiClient { get; set; }
+    [Inject] public required ICryptoService CryptoService { get; set; }
+    [Inject] public required ISignatureService SignatureService { get; set; }
+    [Inject] public required NavigationManager NavigationManager { get; set; }
+
     private readonly SignupFormModel _model = new();
     private List<string> _errors = [];
+    private bool _isLoading = false;
 
     private async Task Submit()
     {
         _errors.Clear();
+        _isLoading = true;
 
-        var signatureSaltBytes = CryptoService.GenerateSalt(16);
+        try
+        {
+            var signupRequest = GenerateSignupRequest();
+
+            var response = await ApiClient.HandleJsonPostAsync("auth/signup", JsonContent.Create(signupRequest));
+            if (response.IsSuccess)
+            {
+                NavigationManager.NavigateTo("login");
+                return;
+            }
+            _errors.Add(response.ErrorMessage ?? "Unexpected error during signup");
+        }
+        finally
+        {
+            _isLoading = false;
+        }
+    }
+
+    private SignupRequest GenerateSignupRequest()
+    {
         var passwordBytes = Encoding.UTF8.GetBytes(_model.Password);
+        var signatureSaltBytes = CryptoService.GenerateSalt(16);
 
         var keyPairBytes = SignatureService.GenerateKeyPair(passwordBytes, signatureSaltBytes, 32);
 
-        var signupRequest = new SignupRequest()
+        return new SignupRequest()
         {
             Username = _model.Username,
             SignatureSaltBase64 = Convert.ToBase64String(signatureSaltBytes),
             EncryptionSaltBase64 = Convert.ToBase64String(CryptoService.GenerateSalt(16)),
             PublicKeyBase64 = Convert.ToBase64String(keyPairBytes.PublicKey)
         };
-
-        var response = await HttpClient.PostAsync("auth/signup", JsonContent.Create(signupRequest));
     }
 }

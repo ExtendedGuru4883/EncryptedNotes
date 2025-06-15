3a4d30d6fddf7752ec495bbb1535a6772415e68a
ExtendedGuru4883 <152730089+ExtendedGuru4883@users.noreply.github.com>
2025-06-15 17:07:17 +0200

    Added endpoint to add notes in NotesController

--- Api/Controllers/NotesController.cs
+++ Api/Controllers/NotesController.cs
@@ -1,15 +1,17 @@
+using AutoMapper;
 using Core.Abstractions.BusinessLogic.Services;
 using EncryptedNotes.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Shared.Dto;
+using Shared.Dto.Requests;
 
 namespace EncryptedNotes.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
 [Authorize]
-public class NotesController(INoteService noteService) : ControllerBase
+public class NotesController(INoteService noteService, IMapper mapper) : ControllerBase
 {
     [HttpGet(nameof(GetAll))]
     [ProducesResponseType(typeof(List<NoteDto>), StatusCodes.Status200OK)]
@@ -18,5 +20,15 @@ public class NotesController(INoteService noteService) : ControllerBase
     public async Task<ActionResult<List<NoteDto>>> GetAll()
     {
         return ServiceResultMapper.ToActionResult(await noteService.GetAllForCurrentUser());
+    }
+    
+    [HttpPost(nameof(Add))]
+    [ProducesResponseType(typeof(List<NoteDto>), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<ActionResult<NoteDto>> Add([FromBody] AddNoteRequest addNoteRequest)
+    {
+        var noteDto = mapper.Map<NoteDto>(addNoteRequest);
+        return ServiceResultMapper.ToActionResult(await noteService.AddAsyncToCurrentUser(noteDto));
     }
 }
